5dd8672f1a0a310729addf4e7c57b9d43f0cd846
redfernj-OSU <81777875+redfernj-OSU@users.noreply.github.com>
2021-04-29 13:29:21 -0700

    added test 1

--- cube.py
+++ cube.py
@@ -1,14 +1,20 @@
+import pytest
+
 
 def volume(x, y, z):
     return x * y * z
 
-def main():
-    x = input("x: ")
-    y = input("y: ")
-    z = input("z: ")
-
-    print("volume: ", volume(int(x), int(y), int(z)))
+def test_answer():
+    assert volume(5, 5, 5) == 125
 
 
-if __name__ == '__main__':
-    main()
+# def main():
+#     x = input("x: ")
+#     y = input("y: ")
+#     z = input("z: ")
+
+#     print("volume: ", volume(int(x), int(y), int(z)))
+
+
+# if __name__ == '__main__':
+#     main()
